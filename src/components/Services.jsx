dbdb8fd2a8fe241f28f144cc9ee1eb291c49cc40
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-09 20:48:20 +0200

    solved errors with icons

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,14 +14,14 @@ const Services = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:cash-stack" color="#ff689b" /></div>
+              <div className="icon"><Icon icon="bi:cash-stack" color="#ff689b" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Lorem Ipsum</a></h4>
               <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:calendar4-week" color="#e9bf06" /></div>
+              <div className="icon"><Icon icon="bi:calendar4-week" color="#e9bf06" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Dolor Sitema</a></h4>
               <p className="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
             </div>
@@ -29,14 +29,14 @@ const Services = () => {
 
           <div className="col-lg-4 col-md-6" data-wow-delay="0.1s">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:chat-text" color="#3fcdc7" /></div>
+              <div className="icon"><Icon icon="bi:chat-text" color="#3fcdc7" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
               <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6" data-wow-delay="0.1s">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:credit-card-2-front" color="#41cf2e" /></div>
+              <div className="icon"><Icon icon="bi:credit-card-2-front" color="#41cf2e" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Magni Dolores</a></h4>
               <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
             </div>
@@ -44,14 +44,14 @@ const Services = () => {
 
           <div className="col-lg-4 col-md-6" data-wow-delay="0.2s">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:globe" color="#d6ff22" /></div>
+              <div className="icon"><Icon icon="bi:globe" color="#d6ff22" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Nemo Enim</a></h4>
               <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6" data-wow-delay="0.2s">
             <div className="icon-box">
-              <div className="icon"><Icon icon="bi:clock" color="#4680ff" width="7000" /></div>
+              <div className="icon"><Icon icon="bi:clock" color="#4680ff" className='icon icon-item' /></div>
               <h4 className="title"><a href="">Eiusmod Tempor</a></h4>
               <p className="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
             </div>
