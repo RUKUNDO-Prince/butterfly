43c511e8f145c3687f693c18cf442d05dee2a21d
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-09 21:04:47 +0200

    solved the errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import './script.js'
+import script from './script.js'
 import { Home, About, Services, Portfolio, Team, Contact, Header, Footer, Counts, Clients, Testimonials, Gallery } from './components/index.js';
 
 const App = () => {
